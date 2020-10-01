c1d24e0ac60cd7013aaf691ff4acb8c3378097b7
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-01 18:04:12 -0400

    Added some request parameters

--- src/main/java/com/stradtkt/controller/DemoController.java
+++ src/main/java/com/stradtkt/controller/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
@@ -25,8 +26,9 @@ public class DemoController {
         return "Hello";
     }
     @GetMapping("welcome")
-    public String welcome(Model model) {
-        model.addAttribute("helloMessage", demoService.getHelloMessage("Kevin"));
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
         log.info("model = {}", model);
         return "welcome";
     }
--- target/todo-list-1.0-SNAPSHOT/WEB-INF/view/welcome.jsp
+++ target/todo-list-1.0-SNAPSHOT/WEB-INF/view/welcome.jsp
@@ -12,6 +12,7 @@
 </head>
 <body>
     <h1>${helloMessage}</h1>
+    <h2>Age parameter is ${age}</h2>
     <h2>${welcomeMessage}</h2>
 </body>
 </html>
